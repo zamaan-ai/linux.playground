376f7fd1fca7d6684b6e6d8e59d1834e7da270ba
Mohd Zamaan Akhtar <akhtarzamaan997@gmail.com>
2026-01-13 07:05:53 +0530

    Time: 100 ms (51.01%) | Memory: 8.6 MB (93.14%) - LeetSync

---
+++ 1-two-sum/two-sum.c
@@ -0,0 +1,19 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    *returnSize = 2;
+    int* result = malloc(2 * sizeof(int));
+    
+    // Nested loops = check every pair
+    for(int i = 0; i < numsSize - 1; i++) {
+        for(int j = i + 1; j < numsSize; j++) {
+            if(nums[i] + nums[j] == target) {
+                result[0] = i;
+                result[1] = j;
+                return result;
+            }
+        }
+    }
+    return result;  // Empty if no solution
+}
